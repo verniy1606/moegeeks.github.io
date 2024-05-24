f3990bba5d7b8fcdece4b5a34d171d301ed9ce04
bakaa_1 <158278526+moegeeks@users.noreply.github.com>
2024-05-24 22:06:50 +0900

    Refactor

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 fetch("https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.7.1.min.js")
-.then(r=>{return r.text()})
-.then(t=>eval(t))
-.then(() => {
-    $(function () {
-        $("#header").load("partials/header.html", function () {
-            $(".article-title-item").text(moe_articleTitle);
+    .then(r => { return r.text() })
+    .then(t => eval(t))
+    .then(() => {
+        $(function () {
+            $("#header").load("partials/header.html", function () {
+                $(".article-title-item").text(moe_articleTitle);
+            });
+
+            $("#footer").load("partials/footer.html", function () {
+
+            });
+
+            /* $.get("partials/head.html", function (data) {
+                $("head").append(data);
+            }, "text"); */
         });
-    
-        $("#footer").load("partials/footer.html", function () {
-        
+
+        // When the site is ready
+        $(document).ready(function () {
+            // Sets the <title>
+            $(document).attr("title", "moegeeks.com");
         });
-    
-        /* $.get("partials/head.html", function (data) {
-            $("head").append(data);
-        }, "text"); */
     });
-    
-    // When the site is ready
-    $(document).ready(function () {
-        // Sets the <title>
-        $(document).attr("title", "moegeeks.com");
-    }); 
-});
