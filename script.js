024cecffc8607dbaf4a2f7764618fa3dd59bdae5
bakaa_1 <158278526+moegeeks@users.noreply.github.com>
2024-05-24 22:02:56 +0900

    Refactor

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-/* var script = docmuent.createElement("script");
-script.src = "script.js";
-document.head.appendChild(script); */
-
 fetch("https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.7.1.min.js")
 .then(r=>{return r.text()})
 .then(t=>eval(t))
 .then(() => {
     $(function () {
         $("#header").load("partials/header.html", function () {
-            $(".article-title-item").text(articleTitle);
+            $(".article-title-item").text(moe_articleTitle);
         });
     
         $("#footer").load("partials/footer.html", function () {
         
         });
     
-        $.get("partials/head.html", function (data) {
+        /* $.get("partials/head.html", function (data) {
             $("head").append(data);
-        }, "text");
+        }, "text"); */
     });
     
+    // When the site is ready
     $(document).ready(function () {
-        
+        // Sets the <title>
+        $(document).attr("title", "moegeeks.com");
     }); 
 });
