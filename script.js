9ed88181c2c527a4991ce33eb131c0e129ff86b8
bakaa_1 <158278526+moegeeks@users.noreply.github.com>
2024-05-24 21:31:19 +0900

    Refactor

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-$(function () {
-    $("#header").load("partials/header.html", function () {
-        $(".article-title-item").text(articleTitle);
-    });
+/* var script = docmuent.createElement("script");
+script.src = "script.js";
+document.head.appendChild(script); */
 
-    $("#footer").load("partials/footer.html", function () {
-        // Do nothing
-    });
-
-    // head.htmlの中身を親に追加
-    $.get("partials/head.html", function (data) {
-        $("head").append(data);
-    }, "text");
-});
-
-$(document).ready(function () {
+fetch("https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.7.1.min.js")
+.then(r=>{return r.text()})
+.then(t=>eval(t))
+.then(() => {
+    $(function () {
+        $("#header").load("partials/header.html", function () {
+            $(".article-title-item").text(articleTitle);
+        });
     
+        $("#footer").load("partials/footer.html", function () {
+        
+        });
+    
+        $.get("partials/head.html", function (data) {
+            $("head").append(data);
+        }, "text");
+    });
+    
+    $(document).ready(function () {
+        
+    }); 
 });
